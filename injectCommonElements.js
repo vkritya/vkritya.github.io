5d21b0112d2b98195492f6699b877e87795699ec
vkritya <v.kristof11@gmail.com>
2020-11-24 04:53:38 +0100

    Refine JS import, add content, add other pages

--- injectCommonElements.js
+++ injectCommonElements.js
@@ -1,3 +1,19 @@
+function inject() {
+  //injectHead();
+  injectHeader();
+  injectFooter();
+}
+
+/*function injectHead() {
+  var target = document.getElementsByTagName("head")[0];
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    target.innerHTML = this.responseText;
+  };
+  xhttp.open("GET", "head.html", true);
+  xhttp.send();
+}*/
+
 function injectHeader() {
   var target = document.getElementById("fejlec");
   xhttp = new XMLHttpRequest();
